5d9ab41b48b7c01928f03537e6b26972c78c1bb7
Precious Olusola <preciousolusola16@gmail.com>
2023-01-18 02:37:26 +0100

    Title update for each section

--- src/features/landing/index.jsx
+++ src/features/landing/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Body from './Body';
 import Footer from './Footer';
 import Header from './Header';
 
 const Landing = () => {
+    
+    useEffect(()=>{
+        const normalTitle = document.title;
+
+        // Update title
+        document.title = `Casa | ${normalTitle}`;
+
+        return ()=>{
+            document.title = normalTitle;
+        }
+
+    });
+
     return (
         <>
 
