1406dec2e0153856bc64d273679ec787e508af63
Precious Olusola <preciousolusola16@gmail.com>
2023-01-19 09:32:29 +0100

    Started auth page design

--- src/features/authentication/index.jsx
+++ src/features/authentication/index.jsx
@@ -5,10 +5,10 @@ import { Outlet } from 'react-router-dom';
 
 const AuthenticationLayout = () => {
   return (
-    <div>
-      <h1>AuthenticationLayout!</h1>
-
-      <Outlet/>
+    <div className='auth__page'>
+      <div className='auth__page--content'>
+        <Outlet/>
+      </div>
     </div>
   )
 }
