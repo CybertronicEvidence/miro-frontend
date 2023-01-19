5f6baf4d208afe6245a545b0a219f9f02dd6fe59
Precious Olusola <preciousolusola16@gmail.com>
2023-01-19 12:13:45 +0100

    Added create account UI

--- constants/configs.js
+++ constants/configs.js
@@ -12,8 +12,35 @@ const LoginFormConfig = {
         placeholder: 'Enter your password'
     }
 }
+const CreateAccountFormConfig = {
+    username:{
+        type: 'text',
+        required: true,
+        label:'Username',
+        placeholder: 'Enter a username'
+    },
+    email:{
+        type: 'email',
+        required: true,
+        label:'Email',
+        placeholder: 'Enter an email'
+    },
+    password:{
+        type: 'password',
+        required: true,
+        label:'Password',
+        placeholder: 'Enter your password'
+    },
+    confirmPassword:{
+        type: 'password',
+        required: true,
+        label:'confirm password',
+        placeholder: 'Confirm your password'
+    }
+}
 
 
 export {
-    LoginFormConfig
+    LoginFormConfig,
+    CreateAccountFormConfig
 }
--- src/features/authentication/Signup.jsx
+++ src/features/authentication/Signup.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import Form, {FormButton, Input} from '../../components/Form';
+import { CreateAccountFormConfig } from '../../../constants/configs';
+import Layout from './Layout';
 
 const SignUp = () => {
+  
   return (
-    <div>SignUp</div>
+    <Layout isLogin={false}>      
+        <Form>
+          <Input config={CreateAccountFormConfig.username}/>
+          <Input config={CreateAccountFormConfig.email}/>
+          <Input config={CreateAccountFormConfig.password}/>
+          <Input config={CreateAccountFormConfig.confirmPassword}/>
+          <FormButton/>
+        </Form>
+    </Layout>
   )
 }
 
