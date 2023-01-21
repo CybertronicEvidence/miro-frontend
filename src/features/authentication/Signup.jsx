9a1a5016cf9726f3b6c275b26fb129a21728aa91
Precious Olusola <preciousolusola16@gmail.com>
2023-01-22 00:28:55 +0100

    Applied update to signup

--- src/features/authentication/Signup.jsx
+++ src/features/authentication/Signup.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import Form, {FormButton, Input} from '../../components/Form';
 import { CreateAccountFormConfig } from '../../../constants/configs';
 import Layout from './Layout';
+import AppContext from '../../../app/context';
 
 const SignUp = () => {
+
+
+    const context = useContext(AppContext);
+
+    const [formData, setFormData] = useState({});
+    const [formError, setFormError] = useState({});
+
 
     useEffect(()=>{
         const normalTitle = document.title;
@@ -16,15 +24,31 @@ const SignUp = () => {
             document.title = normalTitle;
         }
 
-    });
+    }, []);
+
+    const handleSubmit = (e=null)=>{
+    
+    if(e) e.preventDefault();
+
+      console.log("submited", formData);
+    }
+
+    const handleInputChange = (e)=>{
+      e.preventDefault();
+
+      const name = e.target.name;
+      const value = e.target.value;
+
+      setFormData(p=>({...p, [name]:value}));
+    }
   
   return (
     <Layout isLogin={false}>      
-        <Form>
-          <Input config={CreateAccountFormConfig.username}/>
-          <Input config={CreateAccountFormConfig.email}/>
-          <Input config={CreateAccountFormConfig.password}/>
-          <Input config={CreateAccountFormConfig.confirmPassword}/>
+        <Form error={formError.message} handleSubmit={handleSubmit}>
+          <Input config={CreateAccountFormConfig.username} error={formError.username} onInputChange={handleInputChange}/>
+          <Input config={CreateAccountFormConfig.email} error={formError.email} onInputChange={handleInputChange}/>
+          <Input config={CreateAccountFormConfig.password} error={formError.password} onInputChange={handleInputChange}/>
+          <Input config={CreateAccountFormConfig.confirmPassword} error={formError.confirmPassword} onInputChange={handleInputChange}/>
           <FormButton text={"Registrati"}/>
         </Form>
     </Layout>
