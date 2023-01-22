d14c09ff64833777533456cfec0e4d395dbf600e
Precious Olusola <preciousolusola16@gmail.com>
2023-01-23 00:37:32 +0100

    Added empty state to chat area

--- src/features/chat/EmptyState.jsx
+++ src/features/chat/EmptyState.jsx
@@ -1,9 +1,72 @@
 import React from 'react';
-
+import {siteLogo} from '../../../constants/assets';
 
 const EmptyState = ()=>{
     return(
-        <h1>Hello World!</h1>
+        <div className="chat_empty">
+            <div className="logo">
+                <img
+                    src={siteLogo}
+                    alt="Miro"
+                />
+            </div>
+
+            <div className="desp_container">
+
+                <div className="desp">
+                    <b>Esempi di cosa puoi chiedermi</b>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                </div>
+
+                <div className="desp">
+
+                    <b>Quello che poso fare</b>
+
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                </div>
+
+
+                <div className="desp">
+
+                    <b>I miei limiti</b>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                </div>
+
+            </div>
+
+
+            <h3>Connettimi a Siri</h3>
+
+            <div className="ddlr">
+                <span>Lorem ipsum dolor sic amet</span>
+
+                <a href="#">Scopri di piu</a>
+            </div>
+        </div>
     )
 }
 
