9412a84bd3bf643bcba2a9e65870718484cc332a
Precious Olusola <preciousolusola16@gmail.com>
2023-01-23 01:55:57 +0100

    Added logout feature

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,18 @@ import React, { useContext,useEffect }from 'react';
 import { useNavigate } from 'react-router-dom';
 import AppContext from '../../app/context'
 import appUrls from '../../constants/urls';
+import { FormButton } from '../components/Form';
 
-const Modal = ({children})=>{
+const Modal = ({children, backgroundClick})=>{
+
+    const onBackgroundClick = ()=>{
+        if (backgroundClick) return backgroundClick();
+
+        return null;
+    }
+
     return (
-        <div className='appModal'>
+        <div className='appModal' onClick={onBackgroundClick}>
 
             <div className="content">
                 {children}
@@ -37,6 +45,43 @@ export const CheckAuthModal = ({onAuth})=>{
         <Modal>
             <div className="authChecker">
                 <span>Verifica dell'autenticazione</span>
+            </div>
+        </Modal>
+    )
+}
+
+
+export const LogoutModal = ({loggingOut,dismiss})=>{
+
+    const {signOut} = useContext(AppContext);
+    const navigate = useNavigate();
+
+
+    const signOutUser = ()=>{
+        signOut();
+        // navigate(appUrls.home);
+        dismiss();
+    }
+
+    if (!loggingOut) return null;
+
+    return (
+        <Modal backgroundClick={()=>dismiss()}>
+            <div className="authChecker">
+                <h5>Do you want to logout?</h5>
+
+                <div className="actions">
+                    <FormButton 
+                        text={"Cancel"}
+                        onClick={()=>dismiss()}
+                        className="sm-button"
+                    />
+                    <FormButton 
+                        text={"Log out"}
+                        onClick={()=>signOutUser()}
+                        className="sm-button"
+                    />
+                </div>
             </div>
         </Modal>
     )
--- src/features/landing/Header.jsx
+++ src/features/landing/Header.jsx
@@ -1,62 +1,74 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { siteLogo, userIcon } from '../../../constants/assets';
 import appUrls from '../../../constants/urls';
 import AppContext from '../../../app/context';
+import { LogoutModal } from '../../components/Modal';
 
 const Header = () => {
-    const {user} = useContext(AppContext);
-    // console.log(user);
+    const {user} = useContext(AppContext);    
     const navigate = useNavigate();
+    const [isLoggingOut, setIsLoggingOut] = useState(false);
+
+
     return (
-        <section id='header'>
+        <>
+            <LogoutModal loggingOut={isLoggingOut} dismiss={()=>setIsLoggingOut(false)}/>
 
-            <div className='content'>
+            <section id='header'>
 
-                <div className='top'>
-                    <div className='brand'>
-                        <Link to={'/'}>
-                            <img 
-                                src={siteLogo}
-                                alt={"Miro"}
-                            />
-                        </Link>
+                <div className='content'>
+
+                    <div className='top'>
+                        <div className='brand'>
+                            <Link to={'/'}>
+                                <img 
+                                    src={siteLogo}
+                                    alt={"Miro"}
+                                />
+                            </Link>
+                        </div>
+
+
+                        <div 
+                            className='user'
+                            onClick={()=>setIsLoggingOut(true)}
+                            title={user?.token ? "Clicca per uscire" : "Clicca per accedere"}>
+                            <Link to={appUrls.signin} onClick={e=>{if (user) e.preventDefault();}}>
+                                <img 
+                                    src={userIcon}
+                                    alt={"Icona utente"}
+                                />                            
+                            </Link>
+
+                            {
+                                user && <span>{user.username}</span>
+                            }
+                        </div>
                     </div>
 
 
-                    <div className='user' title={user?.token ? "Clicca per uscire" : "Clicca per accedere"}>
-                        <Link to={appUrls.signin}>
-                            <img 
-                                src={userIcon}
-                                alt={"Icona utente"}
-                            />                            
-                        </Link>
+                    <h1 className='grand'>
+                        Il tuo assistente digitale
+                    </h1>
 
-                        <span>{user.username}</span>
+                    <div className='tags'>
+                        <span>100% Gratuito</span>
+                        <span>Risponde in 1 secondo</span>
+                        <span>Disponibile 24/7</span>
+                        <span>100% Gratuito</span>
                     </div>
+
+
+                    <div className='buttin'>
+                        <button onClick={()=> navigate('/chat')}>
+                            inizia
+                        </button>
+                    </div>
+
                 </div>
-
-
-                <h1 className='grand'>
-                    Il tuo assistente digitale
-                </h1>
-
-                <div className='tags'>
-                    <span>100% Gratuito</span>
-                    <span>Risponde in 1 secondo</span>
-                    <span>Disponibile 24/7</span>
-                    <span>100% Gratuito</span>
-                </div>
-
-
-                <div className='buttin'>
-                    <button onClick={()=> navigate('/chat')}>
-                        inizia
-                    </button>
-                </div>
-
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
